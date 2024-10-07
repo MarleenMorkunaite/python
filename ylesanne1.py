709a08bc47136387a566176439a37d0a75b360c7
MarleenMorkunaite <marleen@hkhk.edu.ee>
2024-10-07 15:37:28 +0300

    Add files via upload

---
+++ ylesanne1.py
@@ -0,0 +1,27 @@
+# Ülesanne 1
+
+
+
+import turtle
+
+turtle.fd(100)
+turtle.left(120)
+turtle.fd(100)
+turtle.left(120)
+turtle.fd(100)
+turtle.left(120)
+
+turtle.fd(50)
+turtle.left(60)
+turtle.fd(50)
+turtle.left(120)
+turtle.fd(50)
+turtle.left(120)
+turtle.fd(50)
+turtle.left(120)
+
+
+
+
+
+turtle.done()
