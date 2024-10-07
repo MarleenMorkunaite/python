4769964c000d0d3a57ec2e68b4f564c6c034c823
MarleenMorkunaite <marleen@hkhk.edu.ee>
2024-10-07 15:37:00 +0300

    Add files via upload

---
+++ pythonturtleyl1.py
@@ -0,0 +1,57 @@
+# Marleen Morkunaite
+# 07.10.24
+# Harjutus 1
+
+
+import turtle
+
+#Muudan asukohta
+
+turtle.penup()
+turtle.goto(-300,200)
+turtle.pendown()
+
+
+
+
+
+# kolmnurk
+turtle.fd(100)
+turtle.left(120)
+turtle.fd(100)
+turtle.left(120)
+turtle.fd(100)
+turtle.left(120)
+
+
+#M-täht
+turtle.penup()
+turtle.fd(150)
+turtle.pendown()
+
+turtle.fd(20)
+turtle.left(90)
+turtle.fd(100)
+turtle.right(135)
+turtle.fd(40)
+turtle.left(90)
+turtle.fd(40)
+turtle.right(135)
+turtle.fd(100)
+turtle.left(90)
+turtle.fd(20)
+turtle.left(90)
+turtle.fd(120)
+turtle.left(90)
+turtle.fd(20)
+turtle.left(45)
+turtle.fd(40)
+turtle.right(90)
+turtle.fd(40)
+turtle.left(45)
+turtle.fd(20)
+turtle.left(90)
+turtle.fd(120)
+
+
+turtle.done()
