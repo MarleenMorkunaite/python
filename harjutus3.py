57fe03989e6ca9f98f34b5b2cf36e8a830078359
MarleenMorkunaite <marleen@hkhk.edu.ee>
2024-10-16 11:25:28 +0300

    Create harjutus3.py

---
+++ harjutus3.py
@@ -0,0 +1,57 @@
+#ülesanne 3.6 Marleen
+
+
+
+#ülesanne 3.4
+
+raamatu = pealkiri+" = "+Kevade
+raamatu = autor+" = "+Oskar Luts
+raamat = ("raamatu = pealkiri, raamatu = autor")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import turtle
+"""
+# ülesanne 3.5
+mark, mudel = "bmw","118"
+auto = mark+" "+mudel
+aasta = 1997
+hind = 30.60
+
+print("Minu unistuste auto on",auto,"aastast",aasta,", mille hind on",hind,"eurot.")
+"""
+
+
+
+
+
+"""
+kylje_pikkus = 100
+nurk = 120
+kujundi_varv = "red"
+
+
+for i in range (3):
+    for i in range (3):  
+        turtle.forward(kylje_pikkus)
+        turtle.left(nurk)
+
+    turtle.penup()
+    turtle.forward(kylje_pikkus*1,5)
+    turtle.pendown()
+    """
+turtle.done()
+
+
