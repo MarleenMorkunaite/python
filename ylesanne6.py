8366ddb8426b2f2937c99dbe5bf6ebd1a0402c60
MarleenMorkunaite <marleen@hkhk.edu.ee>
2024-10-18 11:47:57 +0300

    yl6

--- ylesanne6.py
+++ ylesanne6.py
@@ -4,7 +4,7 @@
 import turtle
 import math 
 kordaja = 10
-a = 53
+a = math.radians{53}
 h = 4.4
 x = abs(h / math.tan(a))
 c = math.sqrt(h**2 + x** 2)
@@ -15,7 +15,7 @@ print(x)
 turtle.fd(x*kordaja)
 turtle.left(90)
 turtle.fd(10*kordaja)
-turtle.left(180-66)
+turtle.left(180-(90-53))
 turtle.fd(14*kordaja)
 
 turtle.done()
