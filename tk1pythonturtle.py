86dfba3032ef88afc985048bec27e0fcaa82d75f
MarleenMorkunaite <marleen@hkhk.edu.ee>
2024-11-06 15:52:50 +0200

    tk1

---
+++ tk1pythonturtle.py
@@ -0,0 +1,66 @@
+# TK1 06.11.24 Marleen
+# Kujunid 2 6 1
+
+
+import turtle
+
+# Kujund 2
+
+for i in range(2):
+       turtle.lt(90)
+for i in range(2):
+    turtle.lt(45)
+    turtle.fd(100)
+    turtle.left(90)
+    turtle.fd(100)
+    turtle.left(90)
+    turtle.fd(100)
+    turtle.left(90)
+    turtle.fd(100)
+
+
+# Kujund 6
+'''
+d =100
+turtle.speed(0)
+
+for i in range(4):
+    turtle.lt(90)
+    for i in range(2):
+        turtle.fd(d)
+        turtle.left(90)
+        turtle.fd(d/2)
+        turtle.left(90)
+'''
+
+
+
+
+# Kujund 1
+'''
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(50)
+'''
+
+turtle.done()
--- ylesanne9.py
+++ ylesanne9.py
@@ -36,9 +36,21 @@ print("paaritu arvude summa",sum(paaritu))
 
 
 # Kuva arvud 1-42
-
 #  Arvud, mis jagunevad 3, lisa tekst TIK (näiteks 3 TIK)
 #  Arvud, mis jagunevad 5, lisa tekst TAK (näiteks 5 TAK)
 #  Kui jagunevad mõlemaga, siis lisa tekst TIKTAK (näiteks 15 TIKTAK)
+
+for i range(1,43):
+    if i%3==0:
+print(i, "TIK", end=" ")
+elif i%5==0:
+print(i, "TAK", end=" ")
+
+
+
+
+
+
+
 
 # Leia kõik arvud vahemikus 200 kuni 320, mis jaguvad 7-ga, kuid ei jagu 5-ga. Prindi need arvud komadega eraldatult ühele reale
