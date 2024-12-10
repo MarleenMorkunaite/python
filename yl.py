1d64b2b1cbc8ded794c855b2b86c5c80c72a9f3b
MarleenMorkunaite <marleen@hkhk.edu.ee>
2024-12-10 10:00:09 +0200

    Add files via upload

---
+++ yl.py
@@ -0,0 +1,30 @@
+
+failinimi = input("Vali faili nimi: ")
+
+fail = open(failinimi,"r+")
+
+
+read=fail.readline()
+
+print(read)
+
+
+
+
+
+
+
+
+
+
+
+'''
+def mahlapakkide_arv(kg):
+    arv = int(round(kg * 0.4/3,0))
+    return arv 
+
+
+ounad = int(input("Õunte kogus: "))
+
+print(mahlapakkide_arv(ounad))
+'''
