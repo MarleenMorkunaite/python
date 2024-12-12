d280e777a4ed695799c70596c667a8be0357ce04
MarleenMorkunaite <marleen@hkhk.edu.ee>
2024-12-12 11:41:12 +0200

    Add files via upload

---
+++ tu.py
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 4.3
+# def eelarve(n):
+#     kokku = n * 10 + 55
+#     print(kokku)
+# input("Maksimaalne eelarve: ")
+
+
+
+
+# input("Mitu inimest on kutsutud: ")
+# input("Mitu inimest tuleb: ")
+
+
+
+
+
+# failinimi = input("Vali faili nimi: ")
+
+# fail = open(failinimi,"r+")
+
+
+# read=fail.readline()
+
+# print(read)
+
+
+# 4.2
+# def mahlapakkide_arv(kg):
+#     arv = int(round(kg * 0.4/3,0))
+#     return arv 
+
+
+# ounad = int(input("Õunte kogus: "))
+
+# print(mahlapakkide_arv(ounad))
