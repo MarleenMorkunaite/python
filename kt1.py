ac37d6b2703e4bc3689c88f58c63bd51a3fb9931
MarleenMorkunaite <marleen@hkhk.edu.ee>
2024-12-18 15:41:20 +0200

    Add files via upload

---
+++ kt1.py
@@ -0,0 +1,62 @@
+
+# 5. Shopping List
+	# Create a program that will keep track of items for a shopping list. 
+    # The program should keep asking for new items until nothing is entered (no input followed by enter/return key). 
+    # The program should then display the full shopping list.
+
+
+
+
+# 8. TĆ¤ringud
+# 	kuvatakse korrektne arusaadav kĆ¼simus ja hiljem vastus - 1p
+# 	kasutaja vĆµistleb kahe tĆ¤ringuga arvuti vastu - 1p
+# 	kasutaja teeb pakkumise ning suurima tĆ¤ringupunktisumma vĆµitja saab laual oleva raha endale - 2p
+# 	kood kommenteeritud - 1p
+
+# 18.12.24
+
+
+
+
+
+
+
+
+# 2. Vanused
+# 	loo vanuste loend 1p
+# 	leia numbrite suurim ja vĆ¤ikseim arv  1p
+# 	kogusumma  1p
+# 	keskmine 1p
+
+vanused = [14, 15, 18, 20, 22, 34, 76, 89]
+
+
+
+print(vanused)
+
+
+
+# 11. Salakeel
+# 	sinu programm kĆ¼sib kasutajalt, kas ta soovib salakeelt luua vĆµi tĆµlkida - 1p
+# 	kasutaja sisestab tavalise sĆµna, mis muudetakse salakeeleks - 1p
+# 	kasutaja sisestab salakeeles sĆµna, mis teisendatakse jĆ¤lle normaalseks - 1p
+# 	kood kommenteeritud - 1p
+
+
+
+
+# 18.12.24 
+
+input("Kas sa soovid salakeelt luua või tõlkida: ")
+
+if input:
+    luua = "salakeel"
+    tõlkida = "sõna"
+    
+print(input)
+
+# 13. Koosta programm, mis kontrollib, kas kasutaja poolt sisestatud arv on paaris vĆµi paaritu
+# 	kuvatakse korrektne arusaadav kĆ¼simus ja  vastus - 1p
+# 	eelnev kontroll, kas kasutaja ei lisanud arvu vĆµi pani nulli - 2p
+# 	kood mis teavitab paaris vĆµi paaritust arvust - 2p
+# 	kuvatakse programmi pealkiri - 1p
